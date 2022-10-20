fa0a4f67aa60b79f696a8423a9df3d32acee2e54
RamiroIriarte <ramiro_iriarte003@hotmail.com>
2022-10-19 21:00:45 -0300

    Se desarrolllo la clase Contribuyente

---
+++ Contribuyente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListadoContribuyentes
+{
+    public  class Contribuyente
+    {
+        //Atributos
+        string numeroDeCuenta;
+        string nombre;
+        string deuda;
+        string mesPago;
+        string planDePago;
+
+        //Constructor Vacio
+        public Contribuyente()
+        {
+
+        }
+
+        //Getters y Setters
+        public string NumeroDeCuenta { get; set; }
+        public string Nombre { get; set; }
+        public string Deuda { get; set; }
+        public string MesPago { get; set; }
+        public string PlanDePago { get; set; }
+    }
+}
